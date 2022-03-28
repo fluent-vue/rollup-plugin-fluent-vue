4406a6eb8b98a682e49b9e8bfb6a5de6c51a9246
Ivan Demchuk <ivan.demchuk@gmail.com>
2022-03-28 12:22:57 +0300

    Switch to antfu eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,5 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json'
-  },
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'standard-with-typescript'
-  ],
-  ignorePatterns: ['node_modules/', 'dist/', '*.js']
+  'extends': [
+    '@antfu'
+  ]
 }
